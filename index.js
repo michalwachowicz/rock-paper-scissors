beca7999909a5c0be53330ab9d76710b10fae44b
michalwachowicz <wachowiczmichal1@gmail.com>
2024-06-26 14:15:53 +0200

    Add the human choice logic and choice validator

--- index.js
+++ index.js
@@ -1,4 +1,31 @@
+const choices = ["rock", "paper", "scissors"];
+
+const isChoiceValid = (str) => {
+  if (typeof str != "string") return false;
+
+  for (let choice of choices) {
+    if (str === choice) {
+      return true;
+    }
+  }
+
+  return false;
+};
+
 const getComputerChoice = () => {
-  const choices = ["rock", "paper", "scissors"];
   return choices[Math.floor(Math.random() * choices.length)];
 };
+
+const getHumanChoice = () => {
+  let choice;
+
+  do {
+    choice = prompt(
+      "What's your choice? (rock | paper | scissors)"
+    ).toLowerCase();
+  } while (!isChoiceValid(choice));
+
+  return choice;
+};
+
+console.log(getHumanChoice());
