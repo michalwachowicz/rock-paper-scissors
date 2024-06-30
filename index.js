207529c5a222c4f6989f267e94ddb8a1fa31507f
michalwachowicz <wachowiczmichal1@gmail.com>
2024-06-30 14:29:07 +0200

    Move the round logic to the click listener

--- index.js
+++ index.js
@@ -1,51 +1,35 @@
+const gameButtons = document.querySelector(".gamebutton-container");
+
 const choices = ["rock", "paper", "scissors"];
+
+let humanScore = 0;
+let computerScore = 0;
 
 const isChoiceValid = (str) => typeof str === "string" && choices.includes(str);
 
 const capitalize = (str) =>
   str.charAt(0).toUpperCase() + str.slice(1).toLowerCase();
 
-const playGame = () => {
-  let humanScore = 0;
-  let computerScore = 0;
+const getComputerChoice = () =>
+  choices[Math.floor(Math.random() * choices.length)];
 
-  const playRound = (humanChoice, computerChoice) => {
-    const outcomes = {
-      rock: "scissors",
-      paper: "rock",
-      scissors: "paper",
-    };
-
-    if (humanChoice === computerChoice) {
-      alert("Tie!");
-    } else if (outcomes[humanChoice] === computerChoice) {
-      alert(`You win! ${capitalize(humanChoice)} beats ${computerChoice}!`);
-      humanScore++;
-    } else {
-      alert(`You lose! ${capitalize(computerChoice)} beats ${humanChoice}!`);
-      computerScore++;
-    }
+const playRound = (humanChoice, computerChoice) => {
+  const outcomes = {
+    rock: "scissors",
+    paper: "rock",
+    scissors: "paper",
   };
 
-  for (let i = 0; i < 5; i++) {
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
-
-    playRound(humanChoice, computerChoice);
-  }
-
-  const scoreRestartMessage = `(${humanScore} vs ${computerScore})! Restart the page to play a new game!`;
-  if (humanScore > computerScore) {
-    alert(`You won the game ${scoreRestartMessage}`);
-  } else if (computerScore > humanScore) {
-    alert(`You lost the game ${scoreRestartMessage}`);
+  if (humanChoice === computerChoice) {
+    alert("Tie!");
+  } else if (outcomes[humanChoice] === computerChoice) {
+    alert(`You win! ${capitalize(humanChoice)} beats ${computerChoice}!`);
+    humanScore++;
   } else {
-    alert(`Tie ${scoreRestartMessage}`);
+    alert(`You lose! ${capitalize(computerChoice)} beats ${humanChoice}!`);
+    computerScore++;
   }
 };
-
-// playGame();
-const gameButtons = document.querySelector(".gamebutton-container");
 
 gameButtons.addEventListener("click", (e) => {
   const gameButton = e.target.parentNode;
@@ -53,5 +37,8 @@ gameButtons.addEventListener("click", (e) => {
   if (gameButton.type === "submit" && gameButton.id) {
     const humanChoice = gameButton.id;
     if (!isChoiceValid(humanChoice)) return;
+
+    const computerChoice = getComputerChoice();
+    playRound(humanChoice, computerChoice);
   }
 });
